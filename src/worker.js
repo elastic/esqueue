d1e5d68f74b336c102ff7ee33f29f60afc127813
Joe Fleming <joe.fleming@gmail.com>
2016-05-10 17:23:07 -0700

    swollow errors saving job output, include error in debugging output

--- src/worker.js
+++ src/worker.js
@@ -148,7 +148,8 @@ export default class Job extends events.EventEmitter {
       })
       .catch((err) => {
         if (err.statusCode === 409) return false;
-        throw err;
+        this.debug(`Failure saving job output ${job._id}`, err);
+        this.emit('job_error', err);
       });
     }, (jobErr) => {
       // job execution failed
@@ -158,7 +159,7 @@ export default class Job extends events.EventEmitter {
         return;
       }
 
-      this.debug(`Failure occurred on job ${job._id}`);
+      this.debug(`Failure occurred on job ${job._id}`, jobErr);
       this.emit('job_error', jobErr);
       return this._failJob(job, jobErr.toString());
     });
