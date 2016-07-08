919fec483509f296bbb4bcbf4938f356d8d419ad
Joe Fleming <joe.fleming@gmail.com>
2016-07-08 15:29:05 -0700

    add event types to constants

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -12,4 +12,17 @@ export const defaultSettings = {
   DEFAULT_SETTING_DOCTYPE: 'esqueue',
 };
 
-export default Object.assign({}, jobStatuses, defaultSettings);
+export const events = {
+  EVENT_QUEUE_ERROR: 'error',
+  EVENT_JOB_CREATED: 'job:created',
+  EVENT_JOB_ERROR: 'job:error',
+  EVENT_WORKER_ERROR: 'worker:error',
+  EVENT_WORKER_JOB_CLAIM_ERROR: 'worker:claim job error',
+  EVENT_WORKER_JOB_SEARCH_ERROR: 'worker:pending jobs error',
+  EVENT_WORKER_JOB_UPDATE_ERROR: 'worker:update job error',
+  EVENT_WORKER_JOB_FAIL_ERROR: 'worker:failed job update error',
+  EVENT_WORKER_JOB_EXECUTION_ERROR: 'worker:job execution error',
+  EVENT_WORKER_JOB_TIMEOUT_ERROR: 'worker:job timeout',
+};
+
+export default Object.assign({}, jobStatuses, defaultSettings, events);
