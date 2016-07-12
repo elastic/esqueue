f8db6e1bd311b1a3a52fb9eae4ff05f1574b68d5
Joe Fleming <joe.fleming@gmail.com>
2016-07-12 13:22:40 -0700

    add queue tests for job creation

--- test/src/index.js
+++ test/src/index.js
@@ -1,9 +1,17 @@
 import events from 'events';
 import expect from 'expect.js';
 import sinon from 'sinon';
+import proxyquire from 'proxyquire';
 import { noop, times } from 'lodash';
+import constants from '../../lib/constants';
 import elasticsearchMock from '../fixtures/elasticsearch';
-import Esqueue from '../../lib/index';
+import jobMock from '../fixtures/job';
+import workerMock from '../fixtures/worker';
+
+const Esqueue = proxyquire.noPreserveCache()('../../lib/index', {
+  './job.js': jobMock,
+  './worker.js': workerMock,
+});
 
 describe('Esqueue class', function () {
   let client;
@@ -45,6 +53,47 @@ describe('Esqueue class', function () {
       const pingSpy = sinon.spy(client, 'ping');
       new Esqueue('esqueue', { client });
       sinon.assert.calledOnce(pingSpy);
+    });
+  });
+
+  describe('Adding jobs', function () {
+    let indexName;
+    let jobType;
+    let payload;
+    let queue;
+
+    beforeEach(function () {
+      indexName = 'esqueue-index';
+      jobType = 'test-test';
+      payload = { payload: true };
+      queue = new Esqueue(indexName, { client });
+    });
+
+    it('should pass queue instance, index name, type and payload', function () {
+      const job = queue.addJob(jobType, payload);
+      expect(job.getProp('queue')).to.equal(queue);
+      expect(job.getProp('index')).to.match(new RegExp(indexName));
+      expect(job.getProp('jobType')).to.equal(jobType);
+      expect(job.getProp('payload')).to.equal(payload);
+    });
+
+    it('should pass default settings', function () {
+      const job = queue.addJob(jobType, payload);
+      const options = job.getProp('options');
+      expect(options).to.have.property('timeout', constants.DEFAULT_SETTING_TIMEOUT);
+      expect(options).to.have.property('doctype', constants.DEFAULT_SETTING_DOCTYPE);
+    });
+
+    it('should pass queue index settings', function () {
+      const indexSettings = {
+        index: {
+          number_of_shards: 1
+        }
+      };
+
+      queue = new Esqueue(indexName, { client, indexSettings });
+      const job = queue.addJob(jobType, payload);
+      expect(job.getProp('options')).to.have.property('indexSettings', indexSettings);
     });
   });
 
