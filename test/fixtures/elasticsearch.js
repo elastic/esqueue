67381108d73d41d143413eb4beebbee0cd7a57b4
Joe Fleming <joe.fleming@gmail.com>
2016-07-19 12:51:34 -0700

    add indices.refresh method to mock client

--- test/fixtures/elasticsearch.js
+++ test/fixtures/elasticsearch.js
@@ -6,6 +6,7 @@ function Client() {
   this.indices = {
     create: () => Promise.resolve({ acknowledged: true }),
     exists: () => Promise.resolve(false),
+    refresh: () => Promise.resolve(),
   };
 
   this.transport = {};
